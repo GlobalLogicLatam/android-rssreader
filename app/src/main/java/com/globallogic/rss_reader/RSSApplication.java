ee261479e7ce32b640181284c9b8373bab57cfb9
Diego <rotondalediego@gmail.com>
2015-05-12 09:52:32 -0300

    Add version code and name

--- app/src/main/java/com/globallogic/rss_reader/RSSApplication.java
+++ app/src/main/java/com/globallogic/rss_reader/RSSApplication.java
@@ -1,6 +1,9 @@
 package com.globallogic.rss_reader;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -9,7 +12,7 @@ import com.android.volley.toolbox.Volley;
  * Created by diego.rotondale on 10/05/2015.
  */
 public class RSSApplication extends Application {
-
+    private static final String LOG_TAG = RSSApplication.class.getSimpleName();
     private static RSSApplication instance;
     private RequestQueue mRequestQueue;
 
@@ -21,6 +24,17 @@ public class RSSApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        logVersions();
+    }
+
+    private void logVersions() {
+        try {
+            PackageInfo packageInfo = this.getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_ACTIVITIES);
+            Log.d(LOG_TAG, Integer.toString(packageInfo.versionCode));
+            Log.d(LOG_TAG, packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
     }
 
     public RequestQueue getRequestQueue() {
